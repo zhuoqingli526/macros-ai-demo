4d8c99406f2ce12eadf2d5e66c45f85053e368c4
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-04-29 23:33:32 +0000

    commint

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get("/solutions/blank_form", { :controller => "solutions", :action => "display_form" })
   
+  
   post("/solutions/process_form", { :controller => "solutions", :action => "process_inputs" })
   
 end
