b073666dfafb43627716958913aa820346b16785
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-04-30 00:19:55 +0000

    connect_with_AI

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -2,4 +2,56 @@ class MacrosController < ApplicationController
   def display_form
     render({:template => "macro_templates/new_form"})
   end
+
+  def do_magic
+    @the_description = params.fetch("description_param")
+    @the_image = params.fetch("image_param")
+    @the_image_converted = DataURI.convert(@the_image)
+
+    c = OpenAI::Chat.new
+    c.system("You are an expert nutritionist. Your job is to estimate how many grams of carbohydrates, grams of protein, grams of fat, and total calories are in a meal. You should also add a breakdown of how you arrived at these figures, and any other notes you have. The user will provide either a photo, a description, or both.")
+    c.user(@the_description, image:@the_image)
+    @result = c.assistant!
+
+    chat.schema = '{
+      "name": "nutrition_info",
+      "schema": {
+        "type": "object",
+        "properties": {
+          "carbohydrates": {
+            "type": "number",
+            "description": "Amount of carbohydrates in grams."
+          },
+          "protein": {
+            "type": "number",
+            "description": "Amount of protein in grams."
+          },
+          "fat": {
+            "type": "number",
+            "description": "Amount of fat in grams."
+          },
+          "total_calories": {
+            "type": "number",
+            "description": "Total calories in kcal."
+          },
+          "notes": {
+            "type": "string",
+            "description": "A breakdown of how you arrived at the values, and additional notes."
+          }
+        },
+        "required": [
+          "carbohydrates",
+          "protein",
+          "fat",
+          "total_calories",
+          "notes"
+        ],
+        "additionalProperties": false
+      },
+      "strict": true
+    }'
+
+    render({:template => "macro_templates/results"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get("/blank_form", { :controller => "macros", :action => "display_form" })
 
+  post("/process_inputs", { :controller => "macros", :action => "do_magic" })
+
   # Solutions below. Don't peek until you try it yourself and get stuck!
 
   get("/solutions/blank_form", { :controller => "solutions", :action => "display_form" })
