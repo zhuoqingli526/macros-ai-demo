525bfebf101b28b152f2dd3a46c119b607092475
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-04-30 00:46:03 +0000

    final_version

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -9,11 +9,10 @@ class MacrosController < ApplicationController
     @the_image_converted = DataURI.convert(@the_image)
 
     c = OpenAI::Chat.new
+    c.model = "gpt-4.1-nano"
     c.system("You are an expert nutritionist. Your job is to estimate how many grams of carbohydrates, grams of protein, grams of fat, and total calories are in a meal. You should also add a breakdown of how you arrived at these figures, and any other notes you have. The user will provide either a photo, a description, or both.")
-    c.user(@the_description, image:@the_image)
-    @result = c.assistant!
 
-    chat.schema = '{
+    c.schema = '{
       "name": "nutrition_info",
       "schema": {
         "type": "object",
@@ -50,6 +49,15 @@ class MacrosController < ApplicationController
       },
       "strict": true
     }'
+
+    c.user(@the_description, image: @the_image)
+    @result = c.assistant!
+    
+    @g_carbs = @result.fetch("carbohydrates")
+    @g_protein = @result.fetch("protein")
+    @g_fat = @result.fetch("fat")
+    @kcal = @result.fetch("total_calories")
+    @notes = @result.fetch("notes")
 
     render({:template => "macro_templates/results"})
   end
